1dc2a5a3ca1d04ff5bcfd412cc68cd9b40837f80
HSSBoston <74394277+HSSBoston@users.noreply.github.com>
2021-12-21 14:44:40 -0500

    Update dcode.sh

--- workshops/dcode.sh
+++ workshops/dcode.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+cd ~/iot-python
 curl -sS -O https://raw.githubusercontent.com/HSSBoston/raspi-python/main/workshops/kintone.py
 curl -sS -O https://raw.githubusercontent.com/HSSBoston/raspi-python/main/workshops/iotutils.py
