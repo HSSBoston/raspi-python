3f0ed4161e233b7d73d010d48695a9e96a0fa728
HSSBoston <74394277+HSSBoston@users.noreply.github.com>
2021-12-21 19:17:00 -0500

    Update dcode.sh

--- workshops/dcode.sh
+++ workshops/dcode.sh
@@ -3,3 +3,6 @@
 cd ~/iot-python
 curl -sS -O https://raw.githubusercontent.com/HSSBoston/raspi-python/main/workshops/kintone.py
 curl -sS -O https://raw.githubusercontent.com/HSSBoston/raspi-python/main/workshops/iotutils.py
+curl -sS -O https://raw.githubusercontent.com/HSSBoston/raspi-python/main/workshops/camera2.py
+curl -sS -O https://raw.githubusercontent.com/HSSBoston/raspi-python/main/workshops/camera-infinite-loop.py
+curl -sS -O https://raw.githubusercontent.com/HSSBoston/raspi-python/main/workshops/camera-kintone-infinite-loop.py
