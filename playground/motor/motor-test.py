3ca5753e06f8392e7e46deb706b6eb43c5fb9bfb
HSSBoston <74394277+HSSBoston@users.noreply.github.com>
2024-09-01 15:36:31 -0400

    motor test code revised

--- playground/motor/motor-test.py
+++ playground/motor/motor-test.py
@@ -1,7 +1,14 @@
 import explorerhat, time
 
-explorerhat.motor.one.forward(50)
-time.sleep(3)
+explorerhat.motor.one.forwards(50)
+time.sleep(1)
 explorerhat.motor.one.stop()
+
+time.sleep(3)
+
+explorerhat.motor.one.backwards(50)
+time.sleep(1)
+explorerhat.motor.one.stop()
+
 
 
